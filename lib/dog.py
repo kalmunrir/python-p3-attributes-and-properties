09c8db2532ce84bb3a1a1f8ccc18e4685d452bb9
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-09-13 10:15:10 -0600

    finished dog.py deliverables

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    name = ""
+    breed = ""
+    def getName(self):
+        return self.name
+    def setName(self, name):
+        if (type(name) == str and len(name) >= 1 and len(name) <= 25):
+            self.name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def getBreed(self):
+        return self.breed
+    def setBreed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self.breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    def __init__(self, name="Dog", breed="Pointer"):
+        self.setName(name)
+        self.setBreed(breed)
+
+
+fido = Dog(name="Fido")
+print(fido.getName())
